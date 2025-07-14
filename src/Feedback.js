da646bb43b353e159d7e2c965a85992a307dcfc4
Anastasios Pagiatakis <tasopagiatakis@gmail.com>
2025-07-13 22:51:28 -0400

    I have added a feedback form, I am nearing completion on the first version of the website, but I still need to work out how to actually get the feedback to me from the form

--- src/Feedback.js
+++ src/Feedback.js
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 import './App.css';
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, Button } from "@mui/material";
 
 function FeedbackForm(){
     return(
-        <div>
+        <div className="App-feedback">
+            <Typography variant="h2">Feedback Form</Typography>
             <form>
-                <label> Name
-                    <input type="text" />
-                </label>
-                <label> Please Enter Your Feedback Here:
-                    <input type="text" />
-                </label>
+                <div>
+                    <label>
+                        Your Name
+                    </label>
+                    <br></br>
+                    <input size="34" type="text" placeholder="Enter Your Name" name="name"/>
+                </div>
+                <div>
+                    <label>
+                        Your Feedback
+                    </label>
+                    <br></br>
+                    <textarea rows="4" cols="30" placeholder="Enter Your Feedback Here" id="body"></textarea>
+                </div>
+                <br></br>
+                <Button sx={{bgcolor: "#add8e6", color: "black"}}> Submit </Button>
             </form>
         </div>
     );
